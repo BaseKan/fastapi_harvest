fd231ad967630be74dffc3b322be05487b641dc7
Bas de Kan <basdekan@bass-mbp.kpn>
2022-05-31 15:47:27 +0200

    edit to main

--- src/model_api/main.py
+++ src/model_api/main.py
@@ -23,8 +23,4 @@ app.include_router(movie_data_router.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the simple API."}
-
-
-def restart_api():
-    os.system('sh start_api.sh')
+    return {"message": "Welcome to this simple API."}
