a9e1e54f517c701a64557e55e02fe63d010c8071
Bas <bas.de.kan@alliander.com>
2024-04-19 14:42:08 +0200

    add stages argument to register_model

--- src/model_api/mlops/model_serving.py
+++ src/model_api/mlops/model_serving.py
@@ -1,41 +1,38 @@
-import os
 import logging
 
-from mlflow.tracking import MlflowClient
 import mlflow
 import mlflow.keras
 import tensorflow as tf
+from mlflow.tracking import MlflowClient
 
-from model_api.dataloaders import DataLoader
-from model_api.models.retrieval_model import init_retrieval_model_checkpoint
 from model_api.constants import MODEL_NAME
-from model_api.utils.restart_api import restart_api
-from model_api.predictors.tensorflow_predictor import TensorflowPredictor
+from model_api.dataloaders import DataLoader
 from model_api.mlops.utils import get_latest_registered_model
-
+from model_api.models.retrieval_model import init_retrieval_model_checkpoint
+from model_api.predictors.tensorflow_predictor import TensorflowPredictor
+from model_api.utils.restart_api import restart_api
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
 
-def register_best_model(model_name: str, experiment_name: str, metric: str, stage: str = "Production"):
+def register_best_model(
+    model_name: str, experiment_name: str, metric: str, stage: str = "Production"
+):
     client = MlflowClient()
 
     # Search for all the runs in the experiment with the given experiment ID
-    current_experiment=dict(mlflow.get_experiment_by_name(experiment_name))
-    experiment_id=current_experiment['experiment_id']
+    current_experiment = dict(mlflow.get_experiment_by_name(experiment_name))
+    experiment_id = current_experiment["experiment_id"]
 
     df = mlflow.search_runs([experiment_id], order_by=[f"metrics.{metric} DESC"])
 
     best_run_id = df.at[0, "run_id"]
 
-    mlflow.register_model(
-        model_uri=f"runs:/{best_run_id}/model",
-        name=model_name
-        )
+    mlflow.register_model(model_uri=f"runs:/{best_run_id}/model", name=model_name)
 
     # Get latest version
-    latest_versions = client.get_latest_versions(name=model_name)
+    latest_versions = client.get_latest_versions(name=model_name, stages=["None"])
 
     for version in latest_versions:
         logger.info(f"version: {version.version}, stage: {version.current_stage}")
@@ -45,7 +42,7 @@ def register_best_model(model_name: str, experiment_name: str, metric: str, stag
         name=model_name,
         stage=stage,
         version=latest_versions[0].version,
-        archive_existing_versions=True
+        archive_existing_versions=True,
     )
     restart_api()
 
@@ -53,10 +50,7 @@ def register_best_model(model_name: str, experiment_name: str, metric: str, stag
 def register_model(model_name: str, run_id: str, stage: str = "Production"):
     client = MlflowClient()
 
-    mlflow.register_model(
-        model_uri=f"runs:/{run_id}/model",
-        name=model_name
-        )
+    mlflow.register_model(model_uri=f"runs:/{run_id}/model", name=model_name)
 
     # Get latest version
     latest_versions = client.get_latest_versions(name=model_name)
@@ -70,13 +64,14 @@ def register_model(model_name: str, run_id: str, stage: str = "Production"):
         name=model_name,
         stage=stage,
         version=latest_versions[0].version,
-        archive_existing_versions=True
+        archive_existing_versions=True,
     )
     restart_api()
 
 
-def load_registered_retrieval_model(model_name: str, stage: str = "Production",
-                                    data_loader: DataLoader | None = None) -> tf.keras.Model:
+def load_registered_retrieval_model(
+    model_name: str, stage: str = "Production", data_loader: DataLoader | None = None
+) -> tf.keras.Model:
     client = MlflowClient()
 
     # Search for latest registered model and order by creation timestamp
@@ -85,7 +80,9 @@ def load_registered_retrieval_model(model_name: str, stage: str = "Production",
     logger.info(f"Source path of retrieval model: {registered_model.source}")
     source_path = f"./mlruns{registered_model.source.split('mlruns', 1)[1]}"
 
-    logger.info(f"Latest version of {model_name} model in Production stage: {registered_model.version}")
+    logger.info(
+        f"Latest version of {model_name} model in Production stage: {registered_model.version}"
+    )
 
     logged_model_base_path = source_path.rsplit("/model", 1)[0]
 
@@ -93,8 +90,8 @@ def load_registered_retrieval_model(model_name: str, stage: str = "Production",
 
     logger.info(f"Run id of registered model: {run_id}")
     logger.info(f"Source path of registered model: {source_path}")
-    
-    # extract params/metrics data for run_id in a single dict 
+
+    # extract params/metrics data for run_id in a single dict
     run_data_dict = client.get_run(run_id).data.to_dictionary()
     # Get learning rate and embedding dimension
     learning_rate = run_data_dict["params"]["learning_rate"]
@@ -103,16 +100,19 @@ def load_registered_retrieval_model(model_name: str, stage: str = "Production",
     if not data_loader:
         data_loader = DataLoader()
 
-    retrieval_model = init_retrieval_model_checkpoint(model_base_path=logged_model_base_path,
-                                                      data_loader=data_loader,
-                                                      embedding_dimension=embedding_dimension,
-                                                      learning_rate=learning_rate)
+    retrieval_model = init_retrieval_model_checkpoint(
+        model_base_path=logged_model_base_path,
+        data_loader=data_loader,
+        embedding_dimension=embedding_dimension,
+        learning_rate=learning_rate,
+    )
 
     return retrieval_model
 
 
-def load_registered_predictor_model(model_name: str, stage: str = "Production") -> TensorflowPredictor:
-
+def load_registered_predictor_model(
+    model_name: str, stage: str = "Production"
+) -> TensorflowPredictor:
     # Search for latest registered model and order by creation timestamp
     registered_model = get_latest_registered_model(model_name=MODEL_NAME, stage=stage)
 
@@ -120,7 +120,9 @@ def load_registered_predictor_model(model_name: str, stage: str = "Production") 
 
     source_path = f"./mlruns{registered_model.source.split('mlruns', 1)[1]}/data/model"
     logger.info(f"Source path of registered model: {source_path}")
-    logger.info(f"Latest version of {model_name} model in Production stage: {registered_model.version}")
+    logger.info(
+        f"Latest version of {model_name} model in Production stage: {registered_model.version}"
+    )
 
     return TensorflowPredictor(model_path=source_path)
 
